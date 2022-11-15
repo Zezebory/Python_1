fe0ab73ea2242b5bddde4a49bcea6d50cd5968e6
Zezebory <118151141+Zezebory@users.noreply.github.com>
2022-11-15 11:55:03 +0900

    Python_Study_1.10

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-def tax_calc(money):
-    return money * 0.35
+my_name = "Zezebory"
+my_age = 30
+my_color_eyes = "black"
 
-def pay_tax(tax):
-    print("thank you for paying", tax)
+print(f"Hello I'm {my_name}, I have {my_age} years in the earth, {my_color_eyes} is my eye color")
 
-to_pay = tax_calc(15000000)
-pay_tax(to_pay)
+def make_juice(fruit):
+    return f"{fruit}+juice"
+
+def add_ice(juice):
+    return f"{juice}+ice"
+
+def add_sugar(iced_juice):
+    return f"{iced_juice}+sugar"
+
+juice = make_juice("apple")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+
+print(perfect_juice)
